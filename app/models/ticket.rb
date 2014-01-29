1291bbdb6f8dbafe90771ec4fd0ae2b819110651
Paul Janse van Rensburg <paul@searleconsulting.co.zapaul@searleconsulting.co.za>
2014-01-29 16:03:34 +0200

    Fixed ticket generator

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,17 +3,8 @@ class Ticket
 attr_reader :booking, :return ,:client
 
   def initialize(booking)
-    if booking.has_return?
-      @booking = booking.decorate
-      @return = booking.return.decorate if @booking.return
-    else
-      if booking.return
-        @booking = booking.main.decorate
-        @return = booking.decorate
-      else
-        @booking = booking.decorate
-      end
-    end
+    @booking = booking.main ? booking.main.decorate : booking.decorate
+    @return = @booking.return.decorate if @booking.return
     @client = booking.client.decorate
   end
 
@@ -22,15 +13,16 @@ attr_reader :booking, :return ,:client
   end
 
   def total
-    total = @booking.invoice.total
-    return_total = @return.invoice.total if @return
-    total += (return_total || 0)
+    total = 0
+    total += @booking.invoice.total
+    total += @return.invoice.total if @return
+    total
   end
 
   def total_discount
-    total = @booking.invoice.total_discount
-    return_total = @return.invoice.total_discount if @return
-    total += (return_total || 0)
+    total = 0
+    total += @booking.invoice.total_discount
+    total += @return.invoice.total_discount if @return
+    total
   end
-
 end
