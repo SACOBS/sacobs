03b9281f92a2ad99f83ceb74f700a94561908d31
Paul Janse van Rensburg <paul@searleconsulting.co.zapaul@searleconsulting.co.za>
2014-03-18 11:31:10 +0200

    Fix host mailer heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Sacobs::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options = { :host => 'sacobs.herokuapp.com' }
+
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
