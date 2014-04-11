9ae42b39f47f0a7d091fa236354bc07cc13f851f
Paul Janse van Rensburg <paul@searleconsulting.co.zapaul@searleconsulting.co.za>
2014-04-11 19:39:06 +0200

    Custom inputs for cost, distance and percentage

---
+++ app/inputs/cost_input.rb
@@ -0,0 +1,8 @@
+class CostInput < SimpleForm::Inputs::Base
+  def input
+    template.content_tag(:div, class: 'input-prepend') do
+      template.content_tag(:span, 'R' ,class: "add-on") +
+      @builder.text_field(attribute_name, input_html_options)
+    end
+  end
+end
---
+++ app/inputs/distance_input.rb
@@ -0,0 +1,8 @@
+class DistanceInput < SimpleForm::Inputs::Base
+  def input
+    template.content_tag(:div, class: 'input-append') do
+      @builder.text_field(attribute_name, input_html_options) +
+      template.content_tag(:span, 'Km' ,class: "add-on")
+    end
+  end
+end
