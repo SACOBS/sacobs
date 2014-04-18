f45be9427f0ca6b6111b4bb152767450fe47d5c7
Paul Janse van Rensburg <paul@searleconsulting.co.zapaul@searleconsulting.co.za>
2014-04-18 15:51:53 +0200

    Cache clients form

---
+++ app/helpers/cache_helper.rb
@@ -0,0 +1,7 @@
+module CacheHelper
+  def cache_key_for_clients(clients)
+    count = clients.count
+    max_updated_at = clients.max_by(&:updated_at).try(:utc).try(:to_s, :number)
+    "clients/all-#{count}-#{max_updated_at}"
+  end
+end
