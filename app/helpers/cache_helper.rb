b43f10f21ade76bfff7510c9d6a005ea7d36f029
Paul Janse van Rensburg <paul@searleconsulting.co.zapaul@searleconsulting.co.za>
2014-05-15 13:48:43 +0200

    collection key generation changed

--- app/helpers/cache_helper.rb
+++ app/helpers/cache_helper.rb
@@ -3,7 +3,7 @@ module CacheHelper
   def self.cache_key_for_collection(collection, suffix='')
     ids = collection.pluck(:id).join('-')
     count = collection.model.count
-    max_updated_at = collection.pluck(:updated_at).max
+    max_updated_at = collection.model.pluck(:updated_at).max
     collection_name = collection.model.to_s.downcase.pluralize
     Digest::MD5.hexdigest "#{collection_name}_#{count}/#{ids}-#{max_updated_at.to_i}#{collection.to_sql}"
   end
