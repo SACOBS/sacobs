dce140ce479963d51a93b64eab0d75b0ff7ce8cf
Paul Janse van Rensburg <paul@searleconsulting.co.zapaul@searleconsulting.co.za>
2014-05-27 13:39:29 +0200

    Added Terms and conditions to email ticket template

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -1,9 +1,10 @@
 class TicketMailer < ActionMailer::Base
-  helper :bootstrap
+  helper :bootstrap, :application
   default from: "tickets@sacobs.com"
 
   def send_ticket(booking)
     @ticket = Ticket.new(booking)
+    @settings = Setting.first
     mail to: booking.client.email, subject: 'Your Ticket'
   end
 end
