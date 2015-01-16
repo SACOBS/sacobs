3042b3082dd19c53cacfe52d9454a273d2befc01
Paul Janse van Rensburg <paul@searleconsulting.co.zapaul@searleconsulting.co.za>
2015-01-16 16:44:02 +0200

    Removed bracket from migration

--- db/migrate/20150115081641_add_payment_type_to_payment_details.rb
+++ db/migrate/20150115081641_add_payment_type_to_payment_details.rb
@@ -6,7 +6,7 @@ class AddPaymentTypeToPaymentDetails < ActiveRecord::Migration
     add_column :payment_details, :payment_type, :string
 
     PaymentDetail.all.each do  |pd|
-      pd.payment_type = PaymentType.find(pd.payment_type_id).description)
+      pd.payment_type = PaymentType.find(pd.payment_type_id).description
       pd.save(false)
     end
   end
