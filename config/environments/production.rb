99f48bc787a3695066f5b0f3a298319079748184
Paul Janse van Rensburg <paul@searleconsulting.co.zapaul@searleconsulting.co.za>
2015-02-13 10:58:14 +0200

    Added cache headers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,9 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  #config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  config.static_cache_control = "public, max-age=31536000"
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
