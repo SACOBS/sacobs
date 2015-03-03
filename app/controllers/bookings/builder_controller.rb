198e87f115b4171f5f03fce3b40262146b00833c
Paul Janse van Rensburg <paul@searleconsulting.co.zapaul@searleconsulting.co.za>
2015-03-03 13:36:27 +0200

    Ordering of trips searches

--- app/controllers/bookings/builder_controller.rb
+++ app/controllers/bookings/builder_controller.rb
@@ -15,10 +15,10 @@ module Bookings
 
     def index
       if params[:return] == 'true'
-        @stops = ReturnTripSearch.execute(@booking.stop, @booking.quantity, search_params)
+        @stops = ReturnTripSearch.execute(@booking.stop, @booking.quantity, search_params).order('trips.start_date ASC')
         render partial: 'bookings/builder/return_trips', locals: { booking: @booking, stops: @stops }
       else
-        @stops = TripSearch.execute(search_params)
+        @stops = TripSearch.execute(search_params).order('trips.start_date ASC')
         render partial: 'bookings/builder/trips', locals: { booking: @booking, stops: @stops }
       end
       end
